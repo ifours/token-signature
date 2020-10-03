baff00a0d1a53e2a22583aaf096640893c20a7b8
ifours <ilia.sachok@gmail.com>
2020-10-03 22:10:03 +0300

    initial commit 🐣

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const crypto = require('crypto')
+
+const encode = value => Buffer.from(value).toString('base64')
+const decode = value => Buffer.from(value, 'base64').toString('ascii')
+
+const makeHash = (secret, value) => crypto.createHmac('sha256', secret).update(value).digest('hex')
+
+exports.sign = function sing(secret, value) {
+  return `${encode(makeHash(secret, value))}.${encode(value)}`
+}
+
+exports.unsign = function unsign(secret, token) {
+  const parts = token.split('.')
+
+  if (parts.length !== 2) {
+    throw new Error('Wrong signature')
+  }
+
+  const signature = decode(parts[0])
+  const payload = decode(parts[1])
+
+  const expected = makeHash(secret, token)
+
+  if (!crypto.timingSafeEqual(
+      Buffer.from(signature),
+      Buffer.from(expected),
+    )) {
+    throw new Error('Wrong signature')
+  }
+
+  return payload
+}
